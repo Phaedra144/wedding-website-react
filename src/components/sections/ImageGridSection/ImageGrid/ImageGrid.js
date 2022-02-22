7ab1f50267f3c6f9433bd83616789c2d59c7230d
phaedra144 <bedi.szilvia@gmail.com>
2022-02-22 21:51:06 +0100

    change manifest icon, add react medium zoom to img

--- src/components/sections/ImageGridSection/ImageGrid/ImageGrid.js
+++ src/components/sections/ImageGridSection/ImageGrid/ImageGrid.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import styles from './ImageGrid.module.scss'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 const ImageGrid = ({ images = [] }) => (
   <div className={styles.gridWrapper}>
     {images.map((image) => (
+    <Zoom>
       <img
         src={image.src}
         key={image.src}
         alt={image.caption}
         className={styles.image}
       />
+    </Zoom>
     ))}
   </div>
 )
