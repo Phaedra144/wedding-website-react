ecb8f1c2dade265ad6cbee15b8e1b01814cc7cdf
Zoltán Bedi <zoltan.bedi@gmail.com>
2022-02-24 16:41:41 +0100

    Maps alignment

--- src/components/sections/MapSection/MapLocation/MapLocation.js
+++ src/components/sections/MapSection/MapLocation/MapLocation.js
@@ -5,20 +5,22 @@ import styles from './MapLocation.module.scss'
 import { TrainIcon } from './TrainIcon'
 
 const MapLocation = ({ icon, title, text, hovered }) => {
-  let iconComp = <i className={`icon icon-${icon}`} />;
-/*   if (icon === 'train') {
-    iconComp = <TrainIcon className={styles.train-icon} />
-  } */
+  let iconComp = <i className={`icon icon-${icon}`} />
+  if (icon === 'train') {
+    iconComp = <TrainIcon className={styles.trainIcon} />
+  }
 
   return (
     <div className={styles.wrapper}>
-      <h2 className={classNames(styles.locationTitle, hovered && styles.hovered)}>
+      <h2
+        className={classNames(styles.locationTitle, hovered && styles.hovered)}
+      >
         {iconComp}
         {title}
       </h2>
       {!!text && <ReactMarkdown linkTarget="_blank">{text}</ReactMarkdown>}
     </div>
-  );
+  )
 }
 
 export default MapLocation
--- src/components/sections/MapSection/MapSection.js
+++ src/components/sections/MapSection/MapSection.js
@@ -2,14 +2,7 @@ import React from 'react'
 import MapLocation from './MapLocation'
 import styles from './MapSection.module.scss'
 
-const MapSection = ({
-  id,
-  center = { lat: 47.09705070344482, lng: 19.637465518635402 },
-  zoom = 11,
-  locations,
-  showLocationTextSection = true,
-}) => {
-
+const MapSection = ({ id, locations, showLocationTextSection = true }) => {
   return (
     <section className={styles.section} id={id}>
       {showLocationTextSection && (
@@ -27,8 +20,14 @@ const MapSection = ({
         </div>
       )}
       <div className={styles.subsectionWrapper}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.073042319567!2d19.63047061496746!3d47.09763873099158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474178d605313f6d%3A0xf0d6e5ed0071e7b4!2sTam%C3%A1s%20Tanya%20Kft!5e0!3m2!1shu!2shu!4v1645713144060!5m2!1shu!2shu" 
-        width="600" height="450" style={{border:0}} allowfullscreen="true" loading="lazy"></iframe>
+        <iframe
+          title="Tamás tanya térkép"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.073042319567!2d19.63047061496746!3d47.09763873099158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474178d605313f6d%3A0xf0d6e5ed0071e7b4!2sTam%C3%A1s%20Tanya%20Kft!5e0!3m2!1shu!2shu!4v1645713144060!5m2!1shu!2shu"
+          style={{ border: 0 }}
+          width="100%"
+          allowFullScreen={true}
+          loading="lazy"
+        ></iframe>
       </div>
     </section>
   )
