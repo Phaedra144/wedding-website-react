0dfc560b7132532ce42049d9a595d25bd4c45b37
phaedra144 <bedi.szilvia@gmail.com>
2022-02-24 15:41:58 +0100

    remove MapMarker start adding train icon

--- src/components/sections/MapSection/MapLocation/MapLocation.js
+++ src/components/sections/MapSection/MapLocation/MapLocation.js
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import classNames from 'classnames'
 import styles from './MapLocation.module.scss'
+import { TrainIcon } from './TrainIcon'
 
-const MapLocation = ({ icon, title, text, hovered }) => (
-  <div className={styles.wrapper}>
-    <h2 className={classNames(styles.locationTitle, hovered && styles.hovered)}>
-      <i className={`icon icon-${icon}`} />
-      {title}
-    </h2>
-    {!!text && <ReactMarkdown linkTarget="_blank">{text}</ReactMarkdown>}
-  </div>
-)
+const MapLocation = ({ icon, title, text, hovered }) => {
+  let iconComp = <i className={`icon icon-${icon}`} />;
+/*   if (icon === 'train') {
+    iconComp = <TrainIcon className={styles.train-icon} />
+  } */
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={classNames(styles.locationTitle, hovered && styles.hovered)}>
+        {iconComp}
+        {title}
+      </h2>
+      {!!text && <ReactMarkdown linkTarget="_blank">{text}</ReactMarkdown>}
+    </div>
+  );
+}
 
 export default MapLocation
--- src/components/sections/MapSection/MapSection.js
+++ src/components/sections/MapSection/MapSection.js
@@ -1,7 +1,5 @@
-import React, { useState } from 'react'
-import GoogleMap from 'google-map-react'
+import React from 'react'
 import MapLocation from './MapLocation'
-import MapMarker from './MapMarker'
 import styles from './MapSection.module.scss'
 
 const MapSection = ({
@@ -11,19 +9,6 @@ const MapSection = ({
   locations,
   showLocationTextSection = true,
 }) => {
-  const [hoveredMarkerId, setHoveredMarkerId] = useState('')
-
-  const onMarkerHoverEnter = (key) => {
-    setHoveredMarkerId(key)
-  }
-
-  const onMarkerHoverLeave = () => {
-    setHoveredMarkerId('')
-  }
-
-  const onMarkerClick = (key, markerProps) => {
-    window.open(markerProps.link)
-  }
 
   return (
     <section className={styles.section} id={id}>
@@ -35,38 +20,15 @@ const MapSection = ({
                 icon={location.icon}
                 title={location.title}
                 text={location.text}
-                hovered={location.id === hoveredMarkerId}
+                hovered={location.id}
                 key={location.id}
               />
             ))}
         </div>
       )}
       <div className={styles.subsectionWrapper}>
-        <GoogleMap
-          bootstrapURLKeys={{
-            key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-            language: 'en',
-            region: 'HU',
-          }}
-          defaultCenter={center}
-          defaultZoom={zoom}
-          onChildMouseEnter={onMarkerHoverEnter}
-          onChildMouseLeave={onMarkerHoverLeave}
-          onChildClick={onMarkerClick}
-        >
-          {locations &&
-            locations.map((location) => (
-              <MapMarker
-                lat={location.latitude}
-                lng={location.longitude}
-                icon={location.icon}
-                title={location.title}
-                link={location.link}
-                key={location.id}
-                showTooltip={!showLocationTextSection}
-              />
-            ))}
-        </GoogleMap>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.073042319567!2d19.63047061496746!3d47.09763873099158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474178d605313f6d%3A0xf0d6e5ed0071e7b4!2sTam%C3%A1s%20Tanya%20Kft!5e0!3m2!1shu!2shu!4v1645713144060!5m2!1shu!2shu" 
+        width="600" height="450" style={{border:0}} allowfullscreen="true" loading="lazy"></iframe>
       </div>
     </section>
   )
