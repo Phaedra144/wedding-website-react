d0ca58bf5aedf2866232d1b5012281209ffb6da7
phaedra144 <bedi.szilvia@gmail.com>
2022-02-25 22:09:59 +0100

    fix styles

--- src/components/sections/InstagramWallSection/InstagramWallSection.js
+++ src/components/sections/InstagramWallSection/InstagramWallSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ImageSection from '../ImageSection'
+import Section from '../Section'
 import InstagramWall from './InstagramWall'
 
 const InstagramWallSection = ({ youTubeUrl, ...rest }) => (
